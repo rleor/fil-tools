33265c0a2694ed2881df427be7503de64d4863b3
LeoR <418266037@qq.com>
2022-03-25 13:23:34 +0800

    init

---
+++ snap/config.go
@@ -0,0 +1,42 @@
+package main
+
+// TODO: add toString function
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	Fullnode     ApiInfo `json:"fullnode"`
+	StorageMiner ApiInfo `json:"storageminer"`
+	Limit        int     `json:"limit"`
+}
+
+type ApiInfo struct {
+	Token string `json:"token"`
+	Node  string `json:"node"`
+}
+
+func ParseConfig(cfg_path string) (*Config, error) {
+	if cfg_path == "" {
+		cfg_path = "config.json"
+	}
+
+	f, err := os.Open(cfg_path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	byteValue, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	json.Unmarshal(byteValue, &cfg)
+
+	return &cfg, nil
+}
